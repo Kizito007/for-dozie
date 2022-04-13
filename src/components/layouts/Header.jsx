5e02cc119a30540d45bd6637682b1d51ab16982c
Kizito007 <nwakakelechi49@gmail.com>
2022-04-13 23:01:00 +0100

    started navbar desktop setup

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import logo from "./logo.svg"
+import icon from "./icon.svg"
 import "./layout.css"
 
 const Header = () => {
   return (
-    <div className='header'>
+    <nav className='header'>
         <a class="navbar-brand" href="/">
             <div class="logo-image">
               <img src={logo} class="img-fluid"/>
             </div>
         </a>
-    </div>
+        <ul className='nav-list'>
+          <li><a href="#home">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#resume">Resume</a></li>
+          <li><img src={icon} class="img-icon"/></li>
+        </ul>
+    </nav>
   )
 }
 
