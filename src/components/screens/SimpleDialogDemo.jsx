91b45797c52b74f742f8e72829c498067b79b3e2
Kizito007 <nwakakelechi49@gmail.com>
2022-07-05 17:27:19 +0100

    fixes

--- src/components/screens/SimpleDialogDemo.jsx
+++ src/components/screens/SimpleDialogDemo.jsx
@@ -11,7 +11,7 @@ import Dialog from '@mui/material/Dialog';
 // import PersonIcon from '@mui/icons-material/Person';
 // import AddIcon from '@mui/icons-material/Add';
 // import Typography from '@mui/material/Typography';
-import { purple } from '@mui/material/colors';
+// import { purple } from '@mui/material/colors';
 
 
 const emails = ['username@gmail.com', 'user02@gmail.com'];
@@ -28,7 +28,7 @@ function SimpleDialog(props) {
   };
 
   return (
-    <Dialog sx={{ bgcolor: purple[100], color: purple[100] }} onClose={handleClose} open={open}>
+    <Dialog onClose={handleClose} open={open}>
         <img src={image} />
       {/* <DialogTitle>Set backup account</DialogTitle>
       <List sx={{ pt: 0 }}>
